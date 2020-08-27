b5304715f0979818c8a89e3046f42ac96956458d
kumarutsav7 <kumar.u@vectone.com>
2020-08-27 17:07:55 +0530

    Used Autolayout and set the image of the button.

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -13,14 +13,21 @@ class ViewController: UIViewController {
     let plusPhotoButton : UIButton = {
         let button = UIButton()
         button.backgroundColor = .red
+        let imageName = UIImage(named: "plusphotoButton")
+        button.setImage(imageName, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.addSubview(plusPhotoButton)
-        plusPhotoButton.frame = CGRect(x: 0, y: 0, width: 140, height: 140)
-        plusPhotoButton.center = view.center
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        
     }
 
 
