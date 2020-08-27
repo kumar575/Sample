5b378438c1c018e64cbb3692c859a9f3ef6f5329
kumarutsav7 <kumar.u@vectone.com>
2020-08-27 17:42:45 +0530

    Registration Screen Implemented using StackView.

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -28,7 +28,38 @@ class ViewController: UIViewController {
         tf.font = UIFont.systemFont(ofSize: 14)
         return tf
     }()
-
+    
+    let userNameTextField : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "UserName"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let signUpButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    let passWordTextField : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,13 +69,30 @@ class ViewController: UIViewController {
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
         
-        
-        view.addSubview(emailTextField)
-        emailTextField.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor , constant:  20).isActive = true
-        emailTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
-        emailTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 40).isActive = true
-        emailTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
+        setUpInputFields()
+      
+    }
+    
+  fileprivate  func setUpInputFields() {
+    let greenView = UIView()
+    greenView.backgroundColor = .green
+    
+    let redView = UIView()
+    redView.backgroundColor = .red
+    
+    let stackView = UIStackView(arrangedSubviews: [emailTextField, userNameTextField,passWordTextField, signUpButton])
+    
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.distribution = .fillEqually
+    stackView.axis = .vertical
+    stackView.spacing = 10
+    view.addSubview(stackView)
+    
+    stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor , constant:  20).isActive = true
+    stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+    stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+    stackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    
     }
 
 
