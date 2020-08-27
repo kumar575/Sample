af985f5f36e9c6c235bf83e5577594aad17b9577
kumarutsav7 <kumar.u@vectone.com>
2020-08-27 17:18:01 +0530

    Email text using autolayout

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -18,6 +18,16 @@ class ViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
+    
+    let emailTextField : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +37,13 @@ class ViewController: UIViewController {
         plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        
+        
+        view.addSubview(emailTextField)
+        emailTextField.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor , constant:  20).isActive = true
+        emailTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        emailTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 40).isActive = true
+        emailTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
     }
 
